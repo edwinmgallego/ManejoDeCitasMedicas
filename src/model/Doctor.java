215506f4d857549a38a4e6c7b7125a854f5e685c
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-07-27 21:09:23 -0500

    Definiendo las citas disponibles

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -1,4 +1,6 @@
 package model;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -22,7 +24,7 @@ public class Doctor extends User {
 
 
     ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
-    public void addAvailableAppointment(Date date, String time){
+    public void addAvailableAppointment(String date, String time){
         availableAppointments.add(new Doctor.AvailableAppointment(date,time));
     }
 
@@ -46,9 +48,13 @@ public class Doctor extends User {
         private int id;
         private Date date;
         private String time;
-
-        public AvailableAppointment(Date date, String time) {
-            this.date = date;
+        SimpleDateFormat format= new SimpleDateFormat("dd/MM/yyyy")
+        public AvailableAppointment(String date, String time) {
+            try {
+                this.date = format.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             this.time = time;
         }
 
@@ -62,6 +68,9 @@ public class Doctor extends User {
 
         public Date getDate() {
             return date;
+        }
+        public String getDate(String DATE){
+            return format.format(date);
         }
 
         public void setDate(Date date) {
